3ef063823665bf3329b826feaf7114eb8bb61894
GIMT7 <166819242+GIMT7@users.noreply.github.com>
2025-02-17 11:58:45 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
+
+function App() {
+
+const [tasks, setTasks] = useState([]);
+const addTask = (text) => {
+setTasks ([...tasks, { text, completed: false }]); 
+};
+
+const toggleTask=(index) => {
+
+const newTasks = [...tasks];
+
+newTasks[index].completed =! newTasks[index].completed;
+setTasks (newTasks);
+
+};
+
+const deleteTask = (index) => {
+
+const newTasks = tasks.filter((_, i) => i !== index);
+
+setTasks (newTasks);
+
+};
+
+return (
+
+<div>
+
+<h1>Lista de Tarefas</h1>
+
+<TaskForm onAddTask={addTask} />
+
+<TaskList tasks={tasks} onToggleTask={toggleTask} onDeleteTask={deleteTask} />
+
+</div>
+
+ );
+}
+
+
+
+export default App;
